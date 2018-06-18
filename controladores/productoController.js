3f5493ac61970304822b28f1b768602076e73192
arianazuniga <arianazunigasanjuan@gmail.com>
2018-06-18 00:01:01 -0500

    commit

--- controladores/productoController.js
+++ controladores/productoController.js
@@ -1,5 +1,8 @@
 var models=require('../modelos')
 var sequelize = require('sequelize')
+
+var fs = require('fs');
+var path= require('path');
 const Op = sequelize.Op
 
 function registroProducto(req,res){
@@ -30,7 +33,57 @@ function buscarProductoPorId(req,res){
 			res.status(500).send({message:"Error: "+error});
 		});
 }
+function subirImagen(req, res) {
+	var condicion = req.params.id;
+	var file_name = 'No subido...';
+	console.log("ID"+ condicion)
+	if (req.files){
+		var file_path = req.files.Imagen.path;
+
+		var file_split = file_path.split("\\" );
+		console.log(file_split)
+		var file_name = file_split[1];
+		console.log(file_name)
+		var ext_split = file_name.split('.');//ERR
+		var file_ext = ext_split[1];
+
+		if (file_ext=='png'||file_ext=='jpg'|| file_ext=='gif'){
+
+			models.Producto.update( {Imagen: file_name}, {where: {Id_producto:condicion}})
+				.then(function(){
+					models.Producto.findOne({where:{Id_producto:condicion}})
+						.then(function(productos){
+							if(productos){
+								res.status(200).send(productos)
+							}else{
+								res.status(404).send({message:"No existe el producto"})
+							}
+						})
+				})
+				.catch(function(error){
+					res.status(500).send({message:"Error: "+ error})
+				})
+		}
+	}else{
+		res.status(500).send({message:"Eror"})
+		console.log('Error al cargar')
+	}
+}
+
+function obtenerImagen(req,res){
+	var imageFile= req.params.imageFile;
+	var path_file= './uploads/'+imageFile;
+	fs.exists(path_file, function(exists){
+		if(exists){
+			res.sendFile(path.resolve(path_file));
+		}else{
+			res.status(200).send({message: 'No existe la imagen...'});
+		}
+	});
+}
 module.exports={
 	registroProducto,
-	buscarProductoPorId
+	buscarProductoPorId,
+	subirImagen,
+	obtenerImagen
 }
--- rutas/productoRutas.js
+++ rutas/productoRutas.js
@@ -3,7 +3,13 @@ var md_auth =require('../middlewares/validacion')
 var api=express.Router()
 var productoController=require('../controladores/productoController')
 
+var multipart= require('connect-multiparty');
+var md_upload= multipart({uploadDir: './uploads'});
+
+
 api.post('/registro-producto',md_auth.ensureAuth, productoController.registroProducto)
 api.get('/validar-IdProducto/:id',md_auth.ensureAuth, productoController.buscarProductoPorId)
+api.put('/subir-imagen/:id', [md_auth.ensureAuth, md_upload], productoController.subirImagen);
+api.get('/obtener-imagen/:imageFile', productoController.obtenerImagen);
 
 module.exports=api
